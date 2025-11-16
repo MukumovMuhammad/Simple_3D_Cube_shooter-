f354246848f0bbb2a2780060063d94c2287a3697
MukumovMuhammad <muhammad.bw.03@gmail.com>
2025-11-16 23:03:48 +0500

    made a record table. Now you can see who has how many deaths and kilss

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 @onready var navigation_region_3d: NavigationRegion3D = $"../NavigationRegion3D"
-var Enemy_count : int = 10
+var Enemy_count : int = 9
 const ENEMIES = preload("uid://lrrrysua6ugb")
 @export var player : Cube
 var enemy_names = [
@@ -36,23 +36,47 @@ var enemy_names = [
     "Blinker"
 ]
 
+var record_table = [
+	#["Name", deaths 3, kills 4]
+]
 
 func _ready() -> void:
+	record_table.append([player.name, 0,0])
 	spaw_all_enemies()
+	
 
 
 func spaw_all_enemies():
 	for i in range(Enemy_count):
 		var instance_of_enemy : Cube = ENEMIES.instantiate()
 		instance_of_enemy.name = enemy_names[i]
+		record_table.append([enemy_names[i], 0, 0])
 		instance_of_enemy.global_position = Vector3(randi_range(-30,30), 1, randi_range(-30,30))
 		instance_of_enemy.died.connect(on_enemy_died)
 		navigation_region_3d.add_child(instance_of_enemy)
 		#player.write_info(enemy_names[i] + " was spawed")
+	player.get_node("CanvasLayer/HUD").add_items_to_table(record_table)
 
 
 func on_enemy_died(enemy: Cube, by_who, g_position):
-	player.write_info(by_who + " - kileld - " + enemy.name)
+	player.write_info(by_who + " - killed - " + enemy.name)
+	
+	#Updating the death count
+	var needed_i = find_index_of_array(record_table, enemy.name)
+	record_table[needed_i][1]+=1
+	 #+1 so as in items it updated the death 
+	player.get_node("CanvasLayer/HUD").modify_item(needed_i, record_table[needed_i][1], 1)
+	
+	#Updating the Kill count
+	needed_i = find_index_of_array(record_table, by_who)
+	record_table[needed_i][2]+=1
+	 #+2 so as in items it updated the Kills
+	player.get_node("CanvasLayer/HUD").modify_item(needed_i, record_table[needed_i][2], 2)
+	
+	print_debug("The record table is updated!")
+	for i in record_table:
+		print(" | "+ i[0] + " "+ str(i[1]) + "  " + str(i[2]))
+	
 	var enemy_name : String = enemy.name
 	enemy.queue_free()
 	await get_tree().create_timer(randf_range(1,3)).timeout
@@ -62,11 +86,16 @@ func on_enemy_died(enemy: Cube, by_who, g_position):
 	instance_of_enemy.global_position = Vector3(randi_range(-30,30), 1, randi_range(-30,30))
 	navigation_region_3d.add_child(instance_of_enemy)
 	#player.write_info("Enemy was spawed")
-	
+
+
 
 
 func _on_player_died(Name: Cube, Killer_name: String, g_position: Vector3) -> void:
 	player.write_info("You was killed by " + Killer_name)
+	var needed_i = find_index_of_array(record_table, Name.name)
+	record_table[needed_i][1]+=1
+	needed_i = find_index_of_array(record_table, Killer_name)
+	record_table[needed_i][2]+=1
 	player.visible = false
 	player.set_process(false)
 	player.set_physics_process(false)
@@ -80,3 +109,10 @@ func _on_player_died(Name: Cube, Killer_name: String, g_position: Vector3) -> vo
 	player.inner_Cube_ready()
 	#player.write_info("Player has been respawed!")
 	
+
+
+func find_index_of_array(array: Array, element : String)-> int:
+	for i in range(array.size()):
+		if array[i][0] == element:
+			return i;
+	return 0
--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,6 +1,38 @@
 extends Control
 var logs : Array
 @onready var info_label: Label = $Label
+@onready var table: VBoxContainer = $table
+@onready var items: ItemList = $table/items
+
+
+func _ready() -> void:
+	table.visible = false
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("tab"):
+		table.visible = true
+	else:
+		table.visible = false
+	
+
+func add_items_to_table(array: Array)->void:
+	for i in array:
+		items.add_item(i[0])
+		items.add_item(str(i[1]))
+		items.add_item(str(i[2]))
+	
+	for i in range(items.item_count):
+		items.set_item_disabled(i,true)
+	
+func modify_item(item_i:int, updated_value : int, bias : int):
+	#0 1 2
+	#3 4 5 = 0
+	#6 7 8 = 1
+	#9 10 11 = 2
+	#12 13 14 = 3
+	
+	items.set_item_text(item_i * 3 + 3 + bias, str(updated_value))
 
 func write_to_log(info: String):
 	info_label.text = info + "\n" + info_label.text
